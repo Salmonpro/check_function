bb3b8bd31fe57001be0913fe4d10de4a788616c0
Salmonpro <58506154+Salmonpro@users.noreply.github.com>
2020-10-22 14:19:01 +0300

    Delete remove_palindroms.py

--- remove_palindroms.py
+++
@@ -1,9 +0,0 @@
-def remove_palindroms(spells):
-    for i in spells:
-        a = i.lower()
-        if a == a[::-1]:
-            spells.remove(a)
-    return spells
-
-
-print(remove_palindroms(["abba", "bca", "cyiu"]))
